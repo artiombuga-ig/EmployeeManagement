190144d227fbc711e03664f9ca57b63dd65459ae
artiombuga-ig <artiom.buga@iongroup.com>
2023-11-17 10:07:30 +0200

    Add base Main example

--- src/main/java/dev/buga/Main.java
+++ src/main/java/dev/buga/Main.java
@@ -1,5 +1,9 @@
 package dev.buga;
 
+import dev.buga.data.GenericDAOImpl;
+import dev.buga.entity.Department;
+import dev.buga.entity.Employee;
+import dev.buga.entity.Project;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,12 +11,14 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-//        GenericDAOImpl<Department> genericDAO = context.getBean(GenericDAOImpl.class);
-//
-//        Department department = new Department();
-//        department.setName("IT");
-//        genericDAO.create(department);
-//
+        GenericDAOImpl<Department> departmentDAO = context.getBean("departmentDao", GenericDAOImpl.class);
+        GenericDAOImpl<Employee> employeeDAO = context.getBean("employeeDao", GenericDAOImpl.class);
+        GenericDAOImpl<Project> projectDAO = context.getBean("projectDao", GenericDAOImpl.class);
+
+        Department department = new Department();
+        department.setName("IT");
+        departmentDAO.create(department);
+
 //        context.getBean(GenericDAOImpl.class);
     }
 }
