93d012a3453deef6616ba79692c105aa94245838
artiombuga-ig <artiom.buga@iongroup.com>
2023-11-17 10:07:48 +0200

    Add try-with-resources in GenericDAOImpl

--- src/main/java/dev/buga/data/GenericDAOImpl.java
+++ src/main/java/dev/buga/data/GenericDAOImpl.java
@@ -8,72 +8,76 @@ import org.hibernate.Transaction;
 public class GenericDAOImpl<T> implements GenericDAO<T> {
     private final Class<T> type;
     private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
     public GenericDAOImpl(Class<T> type) {
         this.type = type;
     }
 
     @Override
     public void create(T t) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.getTransaction();
-        try {
-            session.persist(t);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.persist(t);
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null && transaction.isActive()) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
             }
-            e.printStackTrace();
         }
     }
 
     @Override
     public T read(Long id) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.getTransaction();
-        T t = null;
-        try {
-            t = session.get(type, id);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            T t = null;
+            try {
+                t = session.get(type, id);
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
             }
-            e.printStackTrace();
+            return t;
         }
-        return t;
     }
 
     @Override
     public void update(T t) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.getTransaction();
-        try {
-            session.merge(t);
-            transaction.commit();
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                session.merge(t);
+                transaction.commit();
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
             }
-            e.printStackTrace();
         }
-
     }
 
     @Override
     public void delete(Long id) {
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.getTransaction();
-        try {
-            T t = session.get(type, id);
-            if (t != null) {
-                session.remove(t);
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            try {
+                T t = session.get(type, id);
+                if (t != null) {
+                    session.remove(t);
+                }
+            } catch (Exception e) {
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                e.printStackTrace();
             }
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
         }
     }
 }
